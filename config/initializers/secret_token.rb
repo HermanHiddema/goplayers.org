9aba7f73b27ed7649dbbfa6910492a25c8bf213c
Herman Hiddema <herman@wantmymoney.nl>
2013-07-13 20:45:59 +0200

    Clean Rails install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GoplayersOrg::Application.config.secret_key_base = '6c2f782089889b69667be7c3c34ba2dd6f49c315c6a1d07bd858209f4b71606faeb6946f318cb3ec7d5ee46334e7c8bc2bd0bace709218a45a08a09981aa8be9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GoplayersOrg::Application.config.session_store :cookie_store, key: '_goplayers_org_session'
